d0ac3a810d74307180f5531ed628934afde1531e
Nadirbek Nazaraly <nadirbeknazaraly@Air-Nadirbek.Dlink>
2018-04-17 10:33:23 +0600

    Template branch added

--- src/router/router.js
+++ src/router/router.js
@@ -9,26 +9,6 @@ module.exports = (app) => {
 
     // GET
     app.get('/', (request, response) => {
-        let mode      = request.query['hub.mode'];
-        let token     = request.query['hub.verify_token'];
-        let challenge = request.query['hub.challenge'];
-
-        if (mode && token) {
-            if (mode === 'subscribe' && token == process.env.VERIFY_TOKEN) {
-
-                // Logging
-                console.info('GET: Webhook verification successful');
-
-                // Responds with the challenge token from the request
-                response.status(200).send(challenge);
-            } else {
-
-                // Logging
-                console.error('GET: Webhook verification failed');
-
-                // Responds with '403 Forbidden'
-                response.sendStatus(403);
-            }
-        }
+        //
     })
 }
